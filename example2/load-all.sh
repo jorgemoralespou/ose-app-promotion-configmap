0d92710bc37b592712453ce4eb9b5e23e1204471
Jorge Morales <jorgemoralespou@gmail.com>
2016-05-03 18:46:25 +0200

    ui file added

--- example2/load-all.sh
+++ example2/load-all.sh
@@ -2,27 +2,23 @@
 
 # You need 2 users (dev and test)
 
-
-echo "Creating the Development project with user dev ...."
 oc login 10.2.2.2:8443 -u dev -p dev
 oc new-project node-app-dev
 oc create -f configmap-dev.json
 oc create -f node-app-deployment.json
 oc create -f node-app-build.json
 
-
-echo "Granting puller rights for the deployer serviceaccount in node-app-test project"
-# TODO: Why the below and not the above. Also, those credentials seem to wide.
-# This is for the serviceaccount to be able to get the image
-oc policy add-role-to-user edit system:serviceaccount:node-app-test:default
-
-
 oc login 10.2.2.2:8443 -u test -p test
 oc new-project node-app-test
 oc create -f configmap-test.json
 oc create -f node-app-deployment.json
-# This is for the user dev to be able to modify(tag) an ImageStream
-oc policy add-role-to-user edit dev
+
+
+oc login 10.2.2.2:8443 -u admin -p admin
+oc create -f roles.json
+oc adm policy add-role-to-user image-tagger dev -n demo-app-test 
+oc adm policy add-role-to-user image-puller system:serviceaccount:node-app-test:deployer -n demo-app-dev
+
 
 # Login in back as developer
 oc login 10.2.2.2:8443 -u dev -p dev
@@ -39,6 +35,6 @@ echo ""
 echo ""
 echo "If you want to promote the application, you can:"
 echo ""
-echo "   oc tag node-app-dev/node-app:latest node-app-test/node-app:test   "
+echo "   oc tag node-app-dev/node-app:latest node-app-test/node-app:latest   "
 echo ""
 echo ""
